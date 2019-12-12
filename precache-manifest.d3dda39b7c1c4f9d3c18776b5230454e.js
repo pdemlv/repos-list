42dfa45a1680fc70c7d1068937d6d68843997f4a
Pavlo Demydiuk <demydiuk@qarea.com>
2019-12-12 18:20:01 +0200

    Updates

---
+++ precache-manifest.d3dda39b7c1c4f9d3c18776b5230454e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d99e321aecbe8cae4a2d754b8f6685",
+    "url": "//pdemlv.github.io/repos-list/index.html"
+  },
+  {
+    "revision": "b042abd76bb2a6228f76",
+    "url": "//pdemlv.github.io/repos-list/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ba11c514cfbb5b861d2a",
+    "url": "//pdemlv.github.io/repos-list/static/css/main.3a92560e.chunk.css"
+  },
+  {
+    "revision": "b042abd76bb2a6228f76",
+    "url": "//pdemlv.github.io/repos-list/static/js/2.15edfc80.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "//pdemlv.github.io/repos-list/static/js/2.15edfc80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba11c514cfbb5b861d2a",
+    "url": "//pdemlv.github.io/repos-list/static/js/main.0dbfeb06.chunk.js"
+  },
+  {
+    "revision": "d763b5a1c0eeaf722c9d",
+    "url": "//pdemlv.github.io/repos-list/static/js/runtime-main.fc7b26ac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//pdemlv.github.io/repos-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
